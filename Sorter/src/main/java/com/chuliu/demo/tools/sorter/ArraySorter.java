b06feb02362b2e846ba22e5b685c2ae9a8bba0fb
eiuhucl <chu.liu@ericsson.com>
2018-08-14 16:56:13 +0800

    [Sorter module dev]ArraySorter, add insertionSort and selectionSort.

--- Sorter/src/main/java/com/chuliu/demo/tools/sorter/ArraySorter.java
+++ Sorter/src/main/java/com/chuliu/demo/tools/sorter/ArraySorter.java
@@ -5,30 +5,100 @@ package com.chuliu.demo.tools.sorter;
  */
 public class ArraySorter {
 
+    //冒泡
     public static int[] sortByBubble(int[] input){
 
-        for (int i=0;i<input.length-1;i++) {
+        //创建一个副本，并操作副本，不改变输入数组
+        int[] inputCopy = new int[input.length];
+        System.arraycopy(input, 0, inputCopy, 0, input.length);
 
-            for (int j=0;j<input.length-1;j++) {
-                if (input[j]>input[j+1]) {
-                    int temp = input[j];
-                    input[j] = input[j+1];
-                    input[j+1] = temp;
+        //每一次遍历，
+        for (int i=0;i<inputCopy.length-1;i++) {
+
+            for (int j=0;j<inputCopy.length-1;j++) {
+                if (inputCopy[j]>inputCopy[j+1]) {
+                    int temp = inputCopy[j];
+                    inputCopy[j] = inputCopy[j+1];
+                    inputCopy[j+1] = temp;
                 }
             }
         }
 
-        return input;
+        return inputCopy;
     }
 
-    public static int[] sortTest(int[] input){
-        return input;
+    //排序
+    public static int[] sortBySelection(int[] input){
+        //创建一个副本，并操作副本，不改变输入数组
+        int[] inputCopy = new int[input.length];
+        System.arraycopy(input, 0, inputCopy, 0, input.length);
+
+        //每一次遍历，找到一个最小数，放在未排序列表的最前面
+        for (int i=0;i<inputCopy.length-1;i++) {
+
+            int minNumber = inputCopy[i];
+            int indexOfMinNumber = i;
+
+            //找到未排序列表里最小的数字的索引
+            for (int j=i;j<inputCopy.length;j++){
+
+                if (inputCopy[j] < minNumber) {
+                    minNumber = inputCopy[j];
+                    indexOfMinNumber = j;
+                }
+
+            }
+
+            //将最小的数字和未排序列表第一个元素交换位置
+            int temp = inputCopy[i];
+            inputCopy[i] = inputCopy[indexOfMinNumber];
+            inputCopy[indexOfMinNumber] = temp;
+
+        }
+
+        return inputCopy;
+    }
+
+    //插入
+    public static int[] sortByInsertion(int[] input){
+
+        //创建一个副本，并操作副本，不改变输入数组
+        int[] inputCopy = new int[input.length];
+        System.arraycopy(input, 0, inputCopy, 0, input.length);
+
+        //每一次遍历，将未排序列表的第一个元素 插入到 已排序列表的正确位置
+        for (int i=1;i<inputCopy.length;i++) {
+
+            int insertNum = inputCopy[i];
+            int currentIndex = i - 1;
+
+            //While实现，待插入元素和已排序列表的元素 从后往前比较，待插入元素较小则 被比较的数后移一位，并插入到当前被比较数的位置
+            while (currentIndex >= 0 && insertNum < inputCopy[currentIndex]) {
+
+                inputCopy[currentIndex+1] = inputCopy[currentIndex]; // 后移一位
+                inputCopy[currentIndex] = insertNum;
+                currentIndex --;
+            }
+
+            //For实现
+            /*for (int j=i-1;j>=0;j--){
+
+                if (insertNum < inputCopy[j]) {
+                    inputCopy[j+1] = inputCopy[j];
+                    inputCopy[j] = insertNum;
+                }
+            }*/
+        }
+
+        return inputCopy;
+
     }
 
     public static void main(String[] args) {
         int[] testArray = new int[]{2,1,9,5,3,7,6};
-        sortByBubble(testArray);
+        int[] result = sortByInsertion(testArray);
 
-        System.out.println(testArray);
+        for (int num:result)
+        System.out.print(num+":");
     }
 }
--- Sorter/src/test/java/com/chuliu/demo/tools/sorter/ArraySorterTest.java
+++ Sorter/src/test/java/com/chuliu/demo/tools/sorter/ArraySorterTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
 
 /**
  * Created by eiuhucl on 8/9/2018.
+ * Unit test for com.chuliu.demo.tools.sorter.ArraySorter
  */
 public class ArraySorterTest {
 
@@ -15,42 +16,45 @@ public class ArraySorterTest {
 
     @Before
     public void setUp() throws Exception {
-        System.out.println("set up");
+        System.out.println("set up test array");
         testArray = new int[]{2,1,9,5,3,7,6};
     }
 
     @After
     public void tearDown() throws Exception {
-        System.out.println("tear down");
+        System.out.println("tear down test array");
         testArray = null;
     }
 
     @Test
     public void sortByBubble() throws Exception {
-        int[] result = ArraySorter.sortByBubble(testArray);;
+        int[] result = ArraySorter.sortByBubble(testArray);
 
-        if (testArray.length != result.length) {
-            assertTrue("The length of origin array and target array is different!"+testArray.length+":"+result.length,false);
-        }
-
-        for (int i=0;i<result.length-1;i++){
-            if (result[i] > result[i+1]) {
-                assertTrue("The order is wrong, the "+i+" element is "+result[i]+", while "+(i+1)+" element is "+result[i+1],false);
-            }
-        }
+        commonValidateSort(testArray,result);
     }
 
     @Test
-    public void sortTest() throws Exception{
-        int[] result = ArraySorter.sortTest(testArray);
+    public void sortBySelection() throws Exception {
+        int[] result = ArraySorter.sortBySelection(testArray);
 
-        if (testArray.length != result.length) {
-            assertTrue("The length of origin array and target array is different!",false);
+        commonValidateSort(testArray,result);
+    }
+
+    @Test
+    public void sortByInsertion() throws Exception {
+        int[] result = ArraySorter.sortByInsertion(testArray);
+
+        commonValidateSort(testArray,result);
+    }
+
+    private void commonValidateSort(int[] input, int[] output){
+        if (input.length != output.length) {
+            assertTrue("The length of origin array and target array is different!"+input.length+":"+output.length,false);
         }
 
-        for (int i=0;i<result.length-1;i++){
-            if (result[i] > result[i+1]) {
-                assertTrue("The order is wrong, the "+i+" element is "+result[i]+", while "+(i+1)+" element is "+result[i+1],false);
+        for (int i=0;i<output.length-1;i++){
+            if (output[i] > output[i+1]) {
+                assertTrue("The order is wrong, the "+i+" element is "+output[i]+", while "+(i+1)+" element is "+output[i+1],false);
             }
         }
     }
